73f7cf8cd03e3bd173c2abf4206e7902a7ba4dba
NotZer0Two <lyonfan2010@gmail.com>
2024-09-09 08:00:44 +0200

    Aggiungere i file di progetto.

---
+++ AdminToolsExpansion/AdminToolsExpansion.cs
@@ -0,0 +1,39 @@
+﻿using AdminTools;
+using Exiled.API.Features;
+using PlaceholderAPI.API.Abstract;
+
+namespace AdminToolsExpansion
+{
+    public class AdminToolsExpansion : PlaceholderExpansion
+    {
+        public override string Author { get; set; } = "NotZer0Two";
+        public override string Identifier { get; set; } = "admintools";
+        public override string RequiredPlugin { get; set; } = "Admin Tools";
+
+        public override string OnRequest(Player player, string param)
+        {
+            switch (param.ToLower())
+            {
+                case "isoverwatch":
+                    return Main.Overwatch.Contains(player.UserId).ToString();
+
+                case "isjailed":
+                    return Main.JailedPlayers.ContainsKey(player.UserId).ToString();
+
+                case "ismuted":
+                    return Main.RoundStartMutes.Contains(player).ToString();
+
+                case "hasinstakill":
+                    return Main.InstantKill.Contains(player).ToString();
+
+                case "hasprygate":
+                    return Main.PryGate.Contains(player).ToString();
+
+                case "hasbreakdoors":
+                    return Main.BreakDoors.Contains(player).ToString();
+            }
+
+            return null;
+        }
+    }
+}
